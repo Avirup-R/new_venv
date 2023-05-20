f5ed3e53e53130db0d958ad31df2ac8336a8a11e
Avitup-R <avirakshit2002@gmail.com>
2023-05-20 11:59:36 +0530

    another commit

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT= '-e .'
+HYPEN_E_DOT = "-e ."
 
-def get_requirements(file_path) -> List(str):
-    requirements=[]
-    with open (file_path) as file_object:
-        requirements= file_object.readlines()
-        requirements=[req.replace('\n', '') for req in requirements]
+def get_requirements(file_path)-> List[str]:
+    """Doc text for getting requirements"""
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
     return requirements
 
+
 setup(
-    name= "new_venv",
-    version="0.0.1",
-    author ="Avirup",
-    author_email="avirakshit2002@gmail.com",
-    packages=find_packages(),
-    install_requires= get_requirements("requirements.txt")
-
+    name = "mlops-stuff",
+    Version = "0.0.1" ,
+    author = "BoomCam",
+    author_email= "bhavithpbrao@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements("requirements.txt"),
 )
-
-
-
--- src/exception.py
+++ src/exception.py
@@ -9,11 +9,11 @@ def error_message_details(error,error_detail:sys):
         file_name,exc_tb.tb_lineno,str(error))
     return error_message
 class CustomException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        super().__init__(error_message) # inherit the error message
+    def _init_(self,error_message,error_details:sys):
+        super()._init_(error_message) # inherit the error message
         self.error_message = error_message_details(error_message,error_details=error_details)
 
-    def __str__(self):
+    def _str_(self):
         return self.error_message
 
 if __name__=="__main__":
@@ -22,4 +22,3 @@ if __name__=="__main__":
     except Exception as e:
         logging.info('Zero Division error')
         raise CustomException(e,sys)
-        
